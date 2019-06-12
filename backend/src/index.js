020c0855c14d6b5fc03adce7df52369ee2438c47
lukemorales <lukemorales@live.com>
2019-06-12 00:43:50 -0300

    Estrutura Backend Inicial

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+mongoose.connect('mongodb+srv://semana7:semanaOmnistack@cluster0-f0xa6.mongodb.net/test?retryWrites=true&w=majority', {
+	useNewUrlParser: true,
+});
+
+app.use(cors());
+
+app.use((req, res) => {
+	req.io = io;
+
+	next();
+});
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+
+app.use(require('./routes'));
+
+server.listen(3333);
---
+++ backend/src/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const multer = require('multer');
+const PostController = require('./controllers/PostController');
+const LikeController = require('./controllers/LikeController');
+const uploadConfig = require('./config/upload');
+
+const routes = new express.Router();
+const upload = multer(uploadConfig);
+
+routes.get('/posts', PostController.index);
+routes.post('/posts', upload.single('image'), PostController.store);
+
+routes.post('/posts/:id/like', LikeController.store);
+module.exports = routes;
